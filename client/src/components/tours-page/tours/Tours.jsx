5536fdf7de78a44831fabb0443ff1e56ec4efa29
RayaElhss <webdevray07@gmail.com>
2024-08-03 17:42:12 +0300

    add dynamic tours

--- client/src/components/tours-page/tours/Tours.jsx
+++ client/src/components/tours-page/tours/Tours.jsx
@@ -1,31 +1,31 @@
 import styles from '../ToursPage.module.css';
 
-export default function Tours() {
+export default function Tours({ tours }) {
     return (
         <section className={styles.toursList}>
-
-            <div className={styles.tourCard}>
-                <div className="position-relative">
-                    <img src="/img/explore-tour-5.jpg" alt="Family Tour" className={`${styles.tourImg} img-fluid rounded-start`} />
-                    <div className={styles.priceTag}>$649.00</div>
-                </div>
-                <div className={styles.tourDetails}>
-                    <h2>Paris</h2>
-                    <div className={`${styles.offer} ${styles.offerWarning}`}>50% Off</div>
-                    <button className={styles.btn}>View Details</button>
-                </div>
-            </div>
-            <div className={styles.tourCard}>
-                <div className="position-relative">
-                    <img src="/img/explore-tour-4.jpg" alt="Adventure Family Tour" className={`${styles.tourImg} img-fluid rounded-start`} />
-                    <div className={styles.priceTag}>$799.00</div>
-                </div>
-                <div className={styles.tourDetails}>
-                    <h2>Italy</h2>
-                    <div className={`${styles.offer} ${styles.offerInfo}`}>20% Off</div>
-                    <button className={styles.btn}>View Details</button>
-                </div>
-            </div>
+            {tours.length > 0 ? (
+                tours.map((tour) => (
+                    <div key={tour._id} className={styles.tourCard}>
+                        <div className="position-relative">
+                            <img
+                                src={tour.images && tour.images.length > 0 ? tour.images[0] : '/img/default-tour.jpg'} // Fallback to a default image if none available
+                                alt={tour.title}
+                                className={`${styles.tourImg} img-fluid rounded-start`}
+                            />
+                            <div className={styles.priceTag}>${tour.price}</div>
+                        </div>
+                        <div className={styles.tourDetails}>
+                            <h2>{tour.title}</h2>
+                            {tour.offer && (
+                                <div className={`${styles.offer} ${styles.offerInfo}`}>{tour.offer}</div>
+                            )}
+                            <button className={styles.btn}>View Details</button>
+                        </div>
+                    </div>
+                ))
+            ) : (
+                <p>No tours available for this category.</p>
+            )}
         </section>
     );
 }
