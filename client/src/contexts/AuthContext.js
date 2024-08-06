00bf1bf08a9794733c08898b8c21b046754c9ce4
RayaElhss <webdevray07@gmail.com>
2024-08-06 03:04:00 +0300

    Add username to AuthContext for better user management

--- client/src/contexts/AuthContext.js
+++ client/src/contexts/AuthContext.js
@@ -3,6 +3,7 @@ import { createContext } from "react";
 export const AuthContext = createContext({
     userId: '',
     email: '',
+    username: '',
     acessToken: '',
     isAuthenticated: false,
     changeAuthState: (authState = {}) => null
